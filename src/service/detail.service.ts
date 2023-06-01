e8cec5d3a6686fa1128f1927ce3af1c4a7b54438
padoleshital <shitalpadole7@gmail.com>
2023-06-01 20:37:32 +0530

    initial commit
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

---
+++ src/service/detail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailService {
+
+  constructor(private http:HttpClient) { }
+
+  getDetails()
+  {
+    return this.http.get<any>(`${environment.baseUrl}`)
+  }
+ 
+}
